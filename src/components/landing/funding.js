7c287b99e083b204ad7b14f5d60efe2a7448e290
Alicia collymore <hello@aliciacollymore.com>
2020-05-29 00:01:25 +0100

    updated funding copy

--- src/components/landing/funding.js
+++ src/components/landing/funding.js
@@ -11,9 +11,9 @@ const Funding = () => {
         <h3>Support our growth</h3>
 
         <p>We are raising £10,000 to kickstart our business.</p>
-        <p>The funds will be used to launch The Noir Collection, four different styles to suit every occasion. The funds collected off these sales will allow us to develop further products and widen our offering.</p>
-        <p>No donation is too small, and we’ll keep you updated along the journey.</p>
-        <p>Together, we can change the industry, one shoe at a time.</p>
+        <p>The funds will be used to launch The Noir Collection; five different styles, all in black to suit every occasion.</p>
+        <p>No donation is too small and we’ll be sure to keep you updated along the journey.</p>
+        <p>Together, we will change the industry, one shoe at a time.</p>
         <a href="https://natwestbackherbusiness.co.uk/mokkah-shoes-for-women-who-wear-larger-sizes?tk=d44c5f9ca479788f90e32ba3628f6bb9862b0fdb" target='_blank'>Learn More</a>
       </div>
     </div>
